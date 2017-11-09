398e5ddbfb9749374d8a9c5a24a483293504e610
Raevan Gregson <raevangregson@fullsail.edu>
2017-11-08 23:10:14 -0500

    cleaned up if statement with repeated code and used a function instead

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+// these are my four arrays for the four different genres of food, inside each one
+// are 3 objects holding the specific items related to its respective array category
 var drinks = [{
     name:"soda",
     description:" Coke/Sprite",
@@ -9,48 +11,38 @@ var drinks = [{
     name:"beer",
   description:"bud/budlight",
   price:"$3.00"
-},
-{
+},{
   name:"coffee",
 description:"decaf/regular",
 price:"$1.50"
-}
-]
+}]
 
 var snacks = [{
     name:"poptart",
     description:"pastry goodness",
     price:"$2.00"
-  },
-  {
+  },{
     name:"cheeze it's",
   description:"cheesy crackers",
   price:"$3.00"
-},
-{
+},{
   name:"chips",
   description:"BBQ",
   price:"$1.50"
-}
-]
-
-
+}];
 
 var sandwiches = [{
     name:"bologna",
     description:"bologna and bread",
     price:"$4.00"
-  },
-  {
+  },{
     name:"chicken salad sandwich",
   description:"chicken, lettuce and bread",
   price:"$5.00"
-},
-{name:"pbj",
+},{name:"pbj",
 description:"peanut butter and bread",
 price:"$2.50"
-}
-]
+}]
 
 var yummies = [{
     name:"brownie",
@@ -65,71 +57,51 @@ var yummies = [{
 {name:"pie",
 description:"apple",
 price:"$2.50"
-}
-]
+}]
 
-
+//this is the on click event for when a food genre is selected,
 $(".menuOptions").on("click",function(){
+
+//we then hide of the yummies button as their are only 3 specific items per
+//genre
 $(".yummies").hide();
+
+//now we show are 3 spans in each button that will hold
+//the specifics description, price and name which we will
+//just recycle the same span holding the genre name
 $(".itemSpecifics").show();
 
+//this grabs the class of the specific button we click
+//so we know which category of food items array we need
+//to grab holding our food objects
+var className = $(this).attr("class").substring(12);
 
-var className = $(this).attr("class");
-switch (className) {
-  case "menuOptions drinks":
-  $(".drinks .name").text(drinks[0].name);
-  $(".description").text(drinks[0].description);
-  $(".price").text(drinks[0].price);
+//this takes the string and evaluates it as a variable
+// where we then save it back into the variable itself
+//so we can call on it below in our function
+    className = eval(className);
 
-  $(".snacks .name").text(drinks[1].name);
-  $(" .description").text(drinks[1].description);
-  $(" .price").text(drinks[1].price);
+//calling our custom function taking in the class name as
+//a variable aka one of our four arrays above
+displayMenuItems(className);
 
-  $(".sandwiches .name").text(drinks[2].name);
-  $(" .description").text(drinks[2].description);
-  $(" .price").text(drinks[2].price);
-  break;
-  case "menuOptions snacks":
-  $(".drinks .name").text(snacks[0].name);
-  $("  .description").text(snacks[0].description);
-  $("  .price").text(snacks[0].price);
 
-  $(".snacks .name").text(snacks[1].name);
-  $(" .description").text(snacks[1].description);
-  $(" .price").text(snacks[1].price);
+// dependent on which array is called we insert the name into the
+//jquery call this way we can recycle the three sections
+//instead of doing three for each one and testing in a if statement or using
+//a switch.
+function displayMenuItems(className){
+  $(".drinks .name").text(className[0].name);
+  $(".description.drank").text(className[0].description);
+  $(".price.drank").text(className[0].price);
 
-  $(".sandwiches .name").text(snacks[2].name);
-  $(" .description").text(snacks[2].description);
-  $(" .price").text(snacks[2].price);
-  break;
-  case "menuOptions sandwiches":
-  $(".drinks .name").text(sandwiches[0].name);
-  $(".description").text(sandwiches[0].description);
-  $(".price").text(sandwiches[0].price);
+  $(".snacks .name").text(className[1].name);
+  $(".description.snack").text(className[1].description);
+  $(".price.snack").text(className[1].price);
 
-  $(".snacks .name").text(sandwiches[1].name);
-  $(".description").text(sandwiches[1].description);
-  $(".price").text(sandwiches[1].price);
-
-  $(".sandwiches .name").text(sandwiches[2].name);
-  $(".description").text(sandwiches[2].description);
-  $(".price").text(sandwiches[2].price);
-  break;
-  case "menuOptions yummies":
-  $(".drinks .name").text(yummies[0].name);
-  $(".description").text(yummies[0].description);
-  $(".price").text(yummies[0].price);
-
-  $(".snacks .name").text(yummies[1].name);
-  $(".description").text(yummies[1].description);
-  $(".price").text(yummies[1].price);
-
-  $(".sandwiches .name").text(yummies[2].name);
-  $(".description").text(yummies[2].description);
-  $(".price").text(yummies[2].price);
-  break;
-  default:
-  break;
+  $(".sandwiches .name").text(className[2].name);
+  $(".description.sandwich").text(className[2].description);
+  $(".price.sandwich").text(className[2].price);
 }
 
 
